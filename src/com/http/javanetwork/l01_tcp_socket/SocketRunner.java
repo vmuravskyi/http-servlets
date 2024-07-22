ef34ace0fd7bf8c966019fe4f3fc4b712530babe
v.muravskyi <v.muravskyi@gmail.com>
2024-07-22 23:14:40 +0200

    added tcp socket client/server interaction

---
+++ src/com/http/javanetwork/l01_tcp_socket/SocketRunner.java
@@ -0,0 +1,30 @@
+package com.http.javanetwork.l01_tcp_socket;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class SocketRunner {
+
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress inetAddress = Inet4Address.getByName("localhost");
+        try (Socket socket = new Socket(inetAddress, 12345);
+             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+             DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+             Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                String request = scanner.nextLine();
+                dataOutputStream.writeUTF(request);
+                System.out.println("Response form server: " + dataInputStream.readUTF());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/com/http/javanetwork/l01_tcp_socket/SocketServerRunner.java
@@ -0,0 +1,30 @@
+package com.http.javanetwork.l01_tcp_socket;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SocketServerRunner {
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(12345);
+             Socket socket = serverSocket.accept();
+             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+             DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+             Scanner scanner = new Scanner(System.in)) {
+            String request = dataInputStream.readUTF();
+            while (!request.equals("stop")) {
+                System.out.println("Client request: " + request);
+                String response = scanner.nextLine();
+                dataOutputStream.writeUTF(response);
+                request = dataInputStream.readUTF();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
